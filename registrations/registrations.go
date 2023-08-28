8439fcc8db2916a0d7462a23c65da41eccdcb350
Vidhu Kant Sharma <vidhukant@vidhukant.com>
2023-08-28 20:43:55 +0530

    added registration form and its validation

--- registrations/registrations.go
+++ registrations/registrations.go
@@ -9,7 +9,7 @@ import (
 var db *gorm.DB
 func Init() {
 	db = database.DB
-	db.AutoMigrate(&Registration{})
+	db.AutoMigrate(&Registration{}, &RecruitmentRegistration{})
 }
 
 type Registration struct {
@@ -21,4 +21,15 @@ type Registration struct {
 	CollegeID   string         `json:"college_id"`
 	Email       string         `json:"e_mail"`
 	Phone       string         `json:"phone_number"`
+}
+
+type RecruitmentRegistration struct {
+  ID          uint           `gorm:"primaryKey"`
+  CreatedAt   time.Time
+  UpdatedAt   time.Time
+  DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Name        string         `gorm:"not null" json:"name"`
+	CollegeID   string         `gorm:"not null" json:"college_id"`
+	Email       string         `gorm:"unique;not null" json:"e_mail"`
+	Phone       string         `gorm:"unique;not null" json:"phone_number"`
 }
--- registrations/router.go
+++ registrations/router.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"os"
 	"net/http"
+	"fmt"
+	"strings"
 	"github.com/gin-gonic/gin"
+  mysql "github.com/go-mysql/errors"
 )
 
 func Routes(route *gin.Engine) {
@@ -43,6 +46,60 @@ func Routes(route *gin.Engine) {
 			if res.Error != nil {
 				ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
 			  log.Printf("Unexpected error while saving new registration to DB: %v\n", res.Error)
+				return
+			}
+
+			log.Printf("Saved new registration to DB: %d\n", r.ID)
+			ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+		})
+	}
+
+	r := route.Group("/api/registrations/recruitment")
+	{
+		r.GET("/", func(ctx *gin.Context) {
+			var registrations []RecruitmentRegistration
+  	  res := db.Find(&registrations)
+
+			pass, ok := ctx.GetQuery("pass")
+			if !ok || pass != os.Getenv("SECRET") {
+				ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+				ctx.Abort()
+				return
+			}
+
+			if res.Error != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+				ctx.Abort()
+			  log.Printf("Unexpected error while reading subects from DB: %v\n", res.Error)
+				return
+			}
+
+			//ctx.JSON(http.StatusOK, gin.H{"data": registrations})
+      ctx.HTML(http.StatusOK, "registrations.tmpl", gin.H{
+        "total": len(registrations),
+        "registrations": registrations,
+      })
+		})
+
+		r.POST("/", func(ctx *gin.Context) {
+			var r RecruitmentRegistration
+			ctx.Bind(&r)
+
+			res := db.Create(&r)
+			if res.Error != nil {
+				_, myErr := mysql.Error(res.Error)
+				if myErr == mysql.ErrDupeKey {
+
+					split := strings.Split(res.Error.Error(), " ")
+					k := split[len(split)-1]
+					key := k[1:len(k)-1]
+
+				  ctx.JSON(http.StatusConflict, gin.H{"error": fmt.Sprintf("%s already in use", key)})
+			    log.Printf("Conflict while saving new registration to DB: %v\n", res.Error)
+				} else {
+				  ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+			    log.Printf("Unexpected error while saving new registration to DB: %v\n", res.Error)
+				}
 				return
 			}
 
